f1a66545bf7eee49125276ed61b9fd97bb03b78c
junhwang <junhwang77@hotmail.com>
2017-02-13 16:53:19 -0800

    initial push

---
+++ Week1/fundamentaldemo/demos/index3.php
@@ -0,0 +1,25 @@
+<?php
+
+$students = array();
+
+$students[] = "Randall";
+$students[] = "Randall";
+$students[] = "Randall";
+$students[] = "Randall";
+
+
+echo "this is stuent 0:".$students[0].'<br>';
+echo "this is stuent 1:".$students[1].'<br>';
+echo "this is stuent 2:".$students[2].'<br>';
+echo "this is stuent 3:".$students[3].'<br>';
+
+$students2 = array(
+                    array("randall","frisk", 26),
+                    array("michael","firs", 43),
+                    array("charles","hwang",45)
+                );
+echo 'this is the age of 0'. $students2[0][2].'<br>';
+echo 'this is the age of 1'. $students2[1][2].'<br>';
+echo 'this is the age of 2'. $students2[2][2].'<br>';
+
+ ?>
