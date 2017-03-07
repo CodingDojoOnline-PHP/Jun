7cb4f160c9781c0a3c485566f14ba16638f02050
Jun-Hyun Hwang <junhwang@Jun-Hyuns-MBP.hsd1.ca.comcast.net>
2017-03-06 16:42:34 -0800

    week 4 commit

---
+++ Week4_OOP/human.php
@@ -0,0 +1,56 @@
+<?php
+class Human {
+    public $health;
+    public $name;
+    public $clan;
+    public $strength = 3;
+    public $intelligence = 3;
+    public $stealth = 3;
+    public function __construct() {
+        echo "I am alive";
+        $this->health = 100;
+    }
+    public function __get($property){
+        if (property_exists($this, $property)) {
+            return $this->property;
+        }
+    }
+    public function __set($property, $value){
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+        return $this;
+    }
+    public function trashTalk(){
+        echo "You want a piece of me?";
+    }
+    public function attack($human){
+        $this->trashTalk();
+        $luck = rand(0,100);
+        if ($luck * $this->$intelligence > 1000) {
+            if ($luck > $human->stealth) {
+                $human->health -= $this->strength;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    public function get_health(){
+        return $this->health;
+    }
+    public function set_health($health){
+        $this->health = $health;
+    }
+}
+$julius = new Human();
+$julius->set_health(80);
+var_dump($julius);
+$clarkson = new Human();
+var_dump($clarkson);
+
+ ?>
