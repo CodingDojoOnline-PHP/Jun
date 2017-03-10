b6e3c10aaa4b2f09306bd4b101034305e0199030
Jun-Hyun Hwang <junhwang@Jun-Hyuns-MBP.hsd1.ca.comcast.net>
2017-03-09 16:04:39 -0800

    OOP and LinkedLists

--- Week4_OOP/human.php
+++ Week4_OOP/human.php
@@ -47,10 +47,76 @@ class Human {
         $this->health = $health;
     }
 }
-$julius = new Human();
-$julius->set_health(80);
-var_dump($julius);
-$clarkson = new Human();
-var_dump($clarkson);
+/**
+ *
+ */
+class Wizard extends Human
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->clan = "Wizard";
+        $this->strength = 5;
+        $this->intelligence = 40;
+        $this->stealth = 5;
+    }
+    public function heal()
+    {
+        $this->health += 10;
+    }
+    public function trashTalk()
+    {
+        echo "Happiness can be found even in the darkest of times";
+    }
+    public function attack($human)
+    {
+        parent::attack($human);
+        $this->heal();
+    }
+}
+
+class Ninja extends Human
+{
+    public function steal()
+    {
+        $this->stealth += 5;
+    }
+    public function trashTalk()
+    {
+        echo "Now you see me...";
+    }
+    public function attack($human)
+    {
+        parent::attack($human);
+        $this->steal();
+    }
+}
+
+class Samurai extends Human
+{
+    public function sacrifice()
+    {
+        $this->health -= 5;
+        $this->strength += 10;
+    }
+    public function trashTalk()
+    {
+        echo "The flower that blooms in adversity is the most beautiful of all";
+    }
+    public function attack($human)
+    {
+        parent::attack($human);
+        $this->sacrifice();
+    }
+}
+
+$ron = new Wizard();
+$sasuke = new Ninja();
+$kenshen = new Samurai();
+
+echo $ron->health;
+$ron->heal();
+$ron->trashTalk();
+echo $ron->health;
 
  ?>
